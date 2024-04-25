4c7e736737635655926f097d5fad9a1d1bc84e0e
Krishna Saahi Yavana <yavak01@pfw.edu>
2024-04-25 02:37:45 -0400

    added hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import Hero from '@/components/Home/Hero'
 export default function Home() {
   return (
-    <div>
-      <h1>Krishna Saahi</h1>
+    <div className='p-5 sm:px-10 md:px-20'>
+      <Hero />
     </div>
   )
 }
