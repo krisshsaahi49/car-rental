cccf9fe27888d15957220c8943371809b465af52
Krishna Saahi Yavana <yavak01@pfw.edu>
2024-04-26 01:30:25 -0400

    css changes

--- components/Home/Hero.jsx
+++ components/Home/Hero.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Hero() {
     return (
         <div className='grid grid-cols-1 
-        md:grid-cols-2 h-screen items-center justify-center'>
+        md:grid-cols-2 h-screen items-center justify-center border-b'>
             <div>
 
                 <h2 className='text-[40px] md:text-[60px] 
