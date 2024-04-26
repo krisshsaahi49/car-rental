678aed4d3fd973905cb9b0fbe7d5ee328c811914
Krishna Saahi Yavana <yavak01@pfw.edu>
2024-04-26 00:28:05 -0400

    css changes

--- components/Home/CarsFiltersOption.tsx
+++ components/Home/CarsFiltersOption.tsx
@@ -20,10 +20,11 @@ function CarsFiltersOption({ carsList, setBrand, orderCarList }: any) {
   return (
     <div className='mt-10 flex items-center justify-between'>
       <div id="cars-catalog">
-        <h2 className='text-[30px] font-bold'>Cars Catalog</h2>
-        <h2>Explore our cars you might like</h2>
+        <h2 className='text-[40px] font-bold'>Rent a Car</h2>
+        <h2 className='text-[20px] text-gray-500 pr-20 mt-5 mb-5 w-7/8'>Welcome to our premier car rental service! Whether you're traveling for business or pleasure, we offer a diverse range of vehicles to suit your needs. From compact cars perfect for navigating city streets to spacious SUVs ideal for family road trips, we have the perfect ride for every occasion.
+      </h2>
       </div>
-      <div className='flex gap-5'>
+      <div className='flex w-1/2 gap-5'>
         <select className="select select-bordered w-full max-w-xs" defaultValue="" onChange={(e) => orderCarList(e.target.value)}>
           <option disabled value="">Price</option>
           <option value="-1">Low to High</option>
