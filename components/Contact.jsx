7601f7ab64eecedd95e61a731b81bbe8c7510724
Krishna Saahi Yavana <yavak01@pfw.edu>
2024-04-27 11:59:17 -0400

    sonar fixes

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 function Contact() {
     return (
-        <div id="contact" className='border-t'>
-            <h2 id="sell-cars" className='text-[35px] font-bold mt-5'>Contact Us</h2>
-            <h2 className='text-[20px] text-gray-500 pr-20 mt-5 mb-5 '>Feel free to reach out to us for any inquiries or assistance. Our dedicated team is here to make your selling experience smooth and hassle-free.</h2>
-            <div class="flex items-center justify-center p-12">
-                <div class="mx-auto w-full max-w-[550px]">
+        <div id="contact" className="border-t">
+            <h2 id="sell-cars" className="text-3xl font-bold mt-5">Contact Us</h2>
+            <h2 className="text-lg text-gray-500 pr-20 mt-5 mb-5">Feel free to reach out to us for any inquiries or assistance. Our dedicated team is here to make your selling experience smooth and hassle-free.</h2>
+            <div className="flex items-center justify-center p-12">
+                <div className="mx-auto w-full max-w-[550px]">
                     <form action="https://formbold.com/s/3OKbj" method="POST">
-                        <div class="mb-5">
+                        <div className="mb-5">
                             <label
-                                for="name"
-                                class="mb-3 block text-base font-medium text-[#07074D]"
+                                htmlFor="name"
+                                className="mb-3 block text-base font-medium text-[#07074D]"
                             >
                                 Full Name
                             </label>
@@ -20,13 +20,13 @@ function Contact() {
                                 name="name"
                                 id="name"
                                 placeholder="Full Name"
-                                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                             />
                         </div>
-                        <div class="mb-5">
+                        <div className="mb-5">
                             <label
-                                for="email"
-                                class="mb-3 block text-base font-medium text-[#07074D]"
+                                htmlFor="email"
+                                className="mb-3 block text-base font-medium text-[#07074D]"
                             >
                                 Email Address
                             </label>
@@ -35,13 +35,13 @@ function Contact() {
                                 name="email"
                                 id="email"
                                 placeholder="example@domain.com"
-                                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                             />
                         </div>
-                        <div class="mb-5">
+                        <div className="mb-5">
                             <label
-                                for="subject"
-                                class="mb-3 block text-base font-medium text-[#07074D]"
+                                htmlFor="subject"
+                                className="mb-3 block text-base font-medium text-[#07074D]"
                             >
                                 Subject
                             </label>
@@ -50,13 +50,13 @@ function Contact() {
                                 name="subject"
                                 id="subject"
                                 placeholder="Enter your subject"
-                                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                             />
                         </div>
-                        <div class="mb-5">
+                        <div className="mb-5">
                             <label
-                                for="message"
-                                class="mb-3 block text-base font-medium text-[#07074D]"
+                                htmlFor="message"
+                                className="mb-3 block text-base font-medium text-[#07074D]"
                             >
                                 Message
                             </label>
@@ -65,12 +65,12 @@ function Contact() {
                                 name="message"
                                 id="message"
                                 placeholder="Type your message"
-                                class="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                                className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                             ></textarea>
                         </div>
                         <div>
                             <button
-                                class="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none"
+                                className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none"
                             >
                                 Submit
                             </button>
@@ -79,7 +79,7 @@ function Contact() {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Contact
+export default Contact;
--- components/ToastMsg.jsx
+++ components/ToastMsg.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 function ToastMsg({msg}) {
   return (
@@ -10,6 +11,10 @@ function ToastMsg({msg}) {
     </div>
     </div>
   )
+}
+
+ToastMsg.propTypes = {
+  msg: PropTypes.object.isRequired
 }
 
 export default ToastMsg
